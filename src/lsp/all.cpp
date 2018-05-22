9fed2ada6eb8d3ffbea4c9bb4a439e812e01a3a2
Clark Rinker <cgrinker@gmail.com>
2018-05-22 11:29:12 -0700

    Adding file evaluation

--- src/lsp/all.cpp
+++ src/lsp/all.cpp
@@ -10,11 +10,164 @@ using namespace craft::lisp;
 using namespace craft::types;
 using namespace cultlang::uv;
 
+namespace _impl {
+	//https://www.codeguru.com/cpp/cpp/algorithms/strings/article.php/c12759/URI-Encoding-and-Decoding.htm
+	const char SAFE[256] =
+	{
+		/*      0 1 2 3  4 5 6 7  8 9 A B  C D E F */
+		/* 0 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* 1 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* 2 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* 3 */ 1,1,1,1, 1,1,1,1, 1,1,0,0, 0,0,0,0,
+
+		/* 4 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+		/* 5 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,0,
+		/* 6 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+		/* 7 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,0,
+
+		/* 8 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* 9 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* A */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* B */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+
+		/* C */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* D */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* E */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+		/* F */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0
+	};
+
+	const char HEX2DEC[256] =
+	{
+		/*       0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F */
+		/* 0 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 1 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 2 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 3 */  0, 1, 2, 3,  4, 5, 6, 7,  8, 9,-1,-1, -1,-1,-1,-1,
+
+		/* 4 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 5 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 6 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 7 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+		/* 8 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* 9 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* A */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* B */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+		/* C */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* D */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+		/* E */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+	};
+
+	std::string UriEncode(const std::string & sSrc)
+	{
+		const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
+		const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
+		const int SRC_LEN = sSrc.length();
+		unsigned char * const pStart = new unsigned char[SRC_LEN * 3];
+		unsigned char * pEnd = pStart;
+		const unsigned char * const SRC_END = pSrc + SRC_LEN;
+
+		for (; pSrc < SRC_END; ++pSrc)
+		{
+			if (SAFE[*pSrc])
+				*pEnd++ = *pSrc;
+			else
+			{
+				// escape this char
+				*pEnd++ = '%';
+				*pEnd++ = DEC2HEX[*pSrc >> 4];
+				*pEnd++ = DEC2HEX[*pSrc & 0x0F];
+			}
+		}
+
+		std::string sResult((char *)pStart, (char *)pEnd);
+		delete[] pStart;
+		return sResult;
+	}
+
+	std::string UriDecode(const std::string & sSrc)
+	{
+		// Note from RFC1630: "Sequences which start with a percent
+		// sign but are not followed by two hexadecimal characters
+		// (0-9, A-F) are reserved for future extension"
+
+		const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
+		const int SRC_LEN = sSrc.length();
+		const unsigned char * const SRC_END = pSrc + SRC_LEN;
+		// last decodable '%' 
+		const unsigned char * const SRC_LAST_DEC = SRC_END - 2;
+
+		char * const pStart = new char[SRC_LEN];
+		char * pEnd = pStart;
+
+		while (pSrc < SRC_LAST_DEC)
+		{
+			if (*pSrc == '%')
+			{
+				char dec1, dec2;
+				if (-1 != (dec1 = HEX2DEC[*(pSrc + 1)])
+					&& -1 != (dec2 = HEX2DEC[*(pSrc + 2)]))
+				{
+					*pEnd++ = (dec1 << 4) + dec2;
+					pSrc += 3;
+					continue;
+				}
+			}
+
+			*pEnd++ = *pSrc++;
+		}
+
+		// the last 2- chars
+		while (pSrc < SRC_END)
+			*pEnd++ = *pSrc++;
+
+		std::string sResult(pStart, pEnd);
+		delete[] pStart;
+		return sResult;
+	}
+}
+
 instance<Module> cultlang::lsp::make_lsp_bindings(craft::instance<craft::lisp::Namespace> ns, craft::instance<> loader)
 {
 	auto ret = instance<Module>::make(ns, loader);
 	auto sem = instance<CultSemantics>::make(ret);
 	ret->builtin_setSemantics(sem);
+
+	sem->builtin_implementMultiMethod("lsp/safe-require", [](instance<std::string> file) {
+		try
+		{
+			ScopeManipulation::Require(fmt::format("file:{0}", *file));
+		}
+		catch(stdext::exception e)
+		{
+			return instance<std::string>::make(e.what());
+		}
+		catch (std::exception e)
+		{
+			return instance<std::string>::make(e.what());
+		}
+
+		return instance<std::string>::make("Module Loaded");
+	});
+	sem->builtin_implementMultiMethod("lsp/uri/scheme", [](instance<std::string> s) {
+		std::smatch sm;
+		std::regex_match(*s, sm, std::regex("(\\w+)://"));
+		if (sm.size()) return instance<std::string>::make(sm[1]);
+		else return instance<std::string>::make("");
+	});
+	sem->builtin_implementMultiMethod("lsp/uri/filepath", [](instance<std::string> s) {
+		return instance<std::string>::make(std::regex_replace(*s, std::regex("\\w+:///"), ""));
+	});
+
+	sem->builtin_implementMultiMethod("lsp/uri/encode", [](instance<std::string> s) {
+		return instance<std::string>::make(_impl::UriEncode(*s));
+	});
+
+	sem->builtin_implementMultiMethod("lsp/uri/decode", [](instance<std::string> s) {
+		return instance<std::string>::make(_impl::UriDecode(*s));	
+	});
 
 	sem->builtin_implementMultiMethod("lsp/body",
 		[](instance<std::string> s, instance<std::string> out)
